b1bec63ff59d6c8d73b76021f432587e9d1fddee
James Glover <james.glover@sanger.ac.uk>
2017-11-08 11:23:28 +0000

    Add js test suite

---
+++ app/javascript/_test_/components/Hello.spec.js
@@ -0,0 +1,11 @@
+// Import Vue and the component being tested
+import Vue from 'vue'
+import Hello from '../../components/Hello.vue'
+// Here are some Jasmine 2.0 tests, though you can
+// use any test runner / assertion library combo you prefer
+describe('Hello', () => {
+  // Inspect the raw component options
+  it('runs its tests', () => {
+    expect(true).toBe(true)
+  })
+})
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,3 +1,3 @@
 const environment = require('./environment')
-
+environment.plugins.get('Manifest').opts.writeToFileEmit = process.env.NODE_ENV !== 'test'
 module.exports = environment.toWebpackConfig()
